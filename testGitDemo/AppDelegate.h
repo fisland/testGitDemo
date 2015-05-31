c3447c564aafc8dc360434f828f0d22de0b01438
张炯枫 <fisland@fisland-MacBook-pro.local>
2015-05-31 23:42:24 +0800

    Initial Commit

---
+++ testGitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGitDemo
+//
+//  Created by 张炯枫 on 15/5/31.
+//  Copyright (c) 2015年 fisland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
