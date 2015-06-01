cb9405b3b3054f228778f67c8067cacf9e3c8a3e
takjay19 <Jay_ccccccccc@chanWenJaydeMacBook-Pro.local>
2015-06-01 22:24:31 +0800

    test
    Add C++ Files

---
+++ testGitDemo/WJHotView.cpp
@@ -0,0 +1,13 @@
+//
+//  WJHotView.cpp
+//  testGitDemo
+//
+//  Created by chanWenJay on 15/6/1.
+//  Copyright (c) 2015年 fisland. All rights reserved.
+//
+
+#include "WJHotView.h"
+
+void Person::sayHello() {
+    printf("Hello C++");
+}
---
+++ testGitDemo/WJHotView.h
@@ -0,0 +1,21 @@
+//
+//  WJHotView.h
+//  testGitDemo
+//
+//  Created by chanWenJay on 15/6/1.
+//  Copyright (c) 2015年 fisland. All rights reserved.
+//
+
+#ifndef __testGitDemo__WJHotView__
+#define __testGitDemo__WJHotView__
+
+#include <stdio.h>
+
+class Person {
+    
+    
+public:
+    void sayHello();
+};
+
+#endif /* defined(__testGitDemo__WJHotView__) */
