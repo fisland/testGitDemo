b7aa52375baf21c2ede0ed0b2cc273da3b8fbdef
fisland.will <whereisfisland@163.com>
2015-06-01 22:13:44 +0800

    jfpopview

---
+++ testGitDemo/JFPopView.h
@@ -0,0 +1,13 @@
+//
+//  JFPopView.h
+//  testGitDemo
+//
+//  Created by 张炯枫 on 15/6/1.
+//  Copyright (c) 2015年 fisland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JFPopView : UIView
+
+@end
---
+++ testGitDemo/JFPopView.m
@@ -0,0 +1,28 @@
+//
+//  JFPopView.m
+//  testGitDemo
+//
+//  Created by 张炯枫 on 15/6/1.
+//  Copyright (c) 2015年 fisland. All rights reserved.
+//
+
+#import "JFPopView.h"
+
+@implementation JFPopView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+@end
