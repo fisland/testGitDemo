452a2fff88a2bf12ab08e93bfcc84fcf7fbcbabb
fisland.will <whereisfisland@163.com>
2015-06-04 22:38:11 +0800

    代码规范，易读

---
+++ testGitDemo/LoginVC.h
@@ -0,0 +1,13 @@
+//
+//  LoginVC.h
+//  testGitDemo
+//
+//  Created by 张炯枫 on 15/6/4.
+//  Copyright (c) 2015年 fisland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginVC : UIViewController
+
+@end
