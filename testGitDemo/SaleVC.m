295a7e0e687051ea190d7f90c1b6fe1135711fdb
fisland.will <whereisfisland@163.com>
2015-06-07 14:51:06 +0800

    优惠，分享
    优惠页面，分享页面

--- testGitDemo/SaleVC.m
+++ testGitDemo/SaleVC.m
@@ -22,6 +22,7 @@
     UIBarButtonItem *openItem = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStylePlain target:self action:@selector(backButtonPressed)];
     self.navigationItem.leftBarButtonItem = openItem;
     // Do any additional setup after loading the view.
+    [self initSaleUI];
 }
 
 - (void)backButtonPressed {
@@ -35,14 +36,81 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)initSaleUI{
+    UIImageView *cardImgView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 64+50, 170, 100)];
+    cardImgView.center = CGPointMake(self.view.center.x, cardImgView.center.y);
+    cardImgView.image = [UIImage imageNamed:@"black_envelope_reflected"];
+    [self.view addSubview:cardImgView];
+    
+    UILabel *changeLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(cardImgView.frame)+10, IPHONE_WIDTH, 20)];
+    changeLabel.textColor = BlackColor;
+    changeLabel.font = Font(16.0);
+    changeLabel.textAlignment = NSTextAlignmentCenter;
+    changeLabel.text = @"获得优惠洗车机会";
+    [self.view addSubview:changeLabel];
+    
+    UILabel *detailChangeLabel = [[UILabel alloc]initWithFrame:CGRectMake(15, CGRectGetMaxY(changeLabel.frame)+10, IPHONE_WIDTH - 30, 35)];
+    detailChangeLabel.textColor = RGB_A(140);
+    detailChangeLabel.font = Font(13);
+    detailChangeLabel.numberOfLines = 2;
+    detailChangeLabel.textAlignment = NSTextAlignmentCenter;
+    detailChangeLabel.text = @"向朋友发送优惠洗车邀请，您也将获得优惠洗车机会，价值￥XX。";
+    [self.view addSubview:detailChangeLabel];
+    
+    UILabel *couponLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(detailChangeLabel.frame)+40, IPHONE_WIDTH, 15)];
+    couponLabel.text = @"发送您的优惠码";
+    couponLabel.textAlignment = NSTextAlignmentCenter;
+    couponLabel.textColor = RGB_A(202);
+    couponLabel.font = Font(12);
+    [self.view addSubview:couponLabel];
+    
+    UILabel *codeLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(couponLabel.frame)+5, IPHONE_WIDTH, 20)];
+    codeLabel.text = @"UFK89isn";
+    codeLabel.textAlignment = NSTextAlignmentCenter;
+    codeLabel.textColor = BlackColor;
+    codeLabel.font = Font(15);
+    [self.view addSubview:codeLabel];
+    
+    UIButton *emailButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    emailButton.titleLabel.font = Font(18.0);
+    [emailButton setTitle:@"邮箱邀请" forState:UIControlStateNormal];
+    [emailButton setTitleColor:WhiteColor forState:UIControlStateNormal];
+    [emailButton setBackgroundColor:BlackColor];
+    emailButton.frame = CGRectMake(100/2, CGRectGetMaxY(codeLabel.frame)+15, IPHONE_WIDTH - 100/2*2, 44);
+    emailButton.layer.cornerRadius = 5;
+    [emailButton.titleLabel setFont:Font(18.0)];
+    emailButton.tag = 100;
+    [emailButton addTarget:self action:@selector(inviteBtnAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:emailButton];
+    
+    UIButton *messageBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    messageBtn.titleLabel.font = Font(18.0);
+    [messageBtn setTitle:@"短信邀请" forState:UIControlStateNormal];
+    [messageBtn setTitleColor:WhiteColor forState:UIControlStateNormal];
+    [messageBtn setBackgroundColor:default_blue_color];
+    messageBtn.frame = CGRectMake(100/2, CGRectGetMaxY(emailButton.frame)+5, IPHONE_WIDTH - 100/2*2, 44);
+    messageBtn.layer.cornerRadius = 5;
+    [messageBtn.titleLabel setFont:Font(18.0)];
+    messageBtn.tag = 101;
+    [messageBtn addTarget:self action:@selector(inviteBtnAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:messageBtn];
+    
+    UIButton *socialBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    socialBtn.titleLabel.font = Font(18.0);
+    [socialBtn setTitle:@"社交平台邀请" forState:UIControlStateNormal];
+    [socialBtn setTitleColor:WhiteColor forState:UIControlStateNormal];
+    [socialBtn setBackgroundColor:RGB_B(78, 207, 58)];
+    socialBtn.frame = CGRectMake(100/2, CGRectGetMaxY(messageBtn.frame)+5, IPHONE_WIDTH - 100/2*2, 44);
+    socialBtn.layer.cornerRadius = 5;
+    [socialBtn.titleLabel setFont:Font(18.0)];
+    socialBtn.tag = 102;
+    [socialBtn addTarget:self action:@selector(inviteBtnAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:socialBtn];
+    
 }
-*/
-
+#pragma mark - action
+- (void)inviteBtnAction:(UIButton *)sender{
+    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"温馨提醒" message:@"邀请成功" delegate:self cancelButtonTitle:@"确认" otherButtonTitles:nil];
+    [alert show];
+}
 @end
--- testGitDemo/ShareVC.m
+++ testGitDemo/ShareVC.m
@@ -9,7 +9,9 @@
 #import "ShareVC.h"
 
 @interface ShareVC ()
-
+@property (nonatomic, strong) UIButton *weiboShareBtn;
+@property (nonatomic, strong) UIButton *qqShareBtn;
+@property (nonatomic, strong) UIButton *wechatShareBtn;
 @end
 
 @implementation ShareVC
@@ -22,6 +24,7 @@
     UIBarButtonItem *openItem = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStylePlain target:self action:@selector(backButtonPressed)];
     self.navigationItem.leftBarButtonItem = openItem;
     // Do any additional setup after loading the view.
+    [self initShareUI];
 }
 
 - (void)backButtonPressed {
@@ -34,7 +37,65 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+#pragma mark - init ui
+- (void)initShareUI{
+    self.weiboShareBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.weiboShareBtn.titleLabel.font = Font(18.0);
+    [self.weiboShareBtn setTitle:@"微博分享" forState:UIControlStateNormal];
+    [self.weiboShareBtn setTitleColor:WhiteColor forState:UIControlStateNormal];
+    [self.weiboShareBtn setBackgroundColor:BlackColor];
+    self.weiboShareBtn.frame = CGRectMake(100/2, 150, IPHONE_WIDTH - 100/2*2, 44);
+    self.weiboShareBtn.layer.cornerRadius = 5;
+    [self.weiboShareBtn.titleLabel setFont:Font(18.0)];
+    self.weiboShareBtn.tag = 100;
+    [self.weiboShareBtn addTarget:self action:@selector(shareAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.weiboShareBtn];
+    
+    self.wechatShareBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.wechatShareBtn.titleLabel.font = Font(18.0);
+    [self.wechatShareBtn setTitle:@"微信分享" forState:UIControlStateNormal];
+    [self.wechatShareBtn setTitleColor:WhiteColor forState:UIControlStateNormal];
+    [self.wechatShareBtn setBackgroundColor:default_blue_color];
+    self.wechatShareBtn.frame = CGRectMake(100/2, CGRectGetMaxY(self.weiboShareBtn.frame) +20, IPHONE_WIDTH - 100/2*2, 44);
+    self.wechatShareBtn.layer.cornerRadius = 5;
+    [self.wechatShareBtn.titleLabel setFont:Font(18.0)];
+    self.wechatShareBtn.tag = 101;
+    [self.wechatShareBtn addTarget:self action:@selector(shareAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.wechatShareBtn];
+    
+    self.qqShareBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.qqShareBtn.titleLabel.font = Font(18.0);
+    [self.qqShareBtn setTitle:@"QQ分享" forState:UIControlStateNormal];
+    [self.qqShareBtn setTitleColor:WhiteColor forState:UIControlStateNormal];
+    [self.qqShareBtn setBackgroundColor:RGB_B(78, 207, 58)];
+    self.qqShareBtn.frame = CGRectMake(100/2, CGRectGetMaxY(self.wechatShareBtn.frame)+20, IPHONE_WIDTH - 100/2*2, 44);
+    self.qqShareBtn.layer.cornerRadius = 5;
+    self.qqShareBtn.tag = 102;
+    [self.qqShareBtn.titleLabel setFont:Font(18.0)];
+    [self.qqShareBtn addTarget:self action:@selector(shareAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.qqShareBtn];
+}
+#pragma mark - action
+- (void)shareAction:(UIButton *)sender{
+    NSString *shareStr = nil;
+    switch (sender.tag) {
+        case 100:
+            shareStr = @"微博分享成功";
+            break;
+            
+        case 101:
+            shareStr = @"微信分享成功";
+            break;
+            
+        case 102:
+            shareStr = @"QQ分享成功";
+            break;
+        default:
+            break;
+    }
+    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"温馨提示" message:shareStr delegate:self cancelButtonTitle:@"确认" otherButtonTitles: nil];
+    [alert show];
+}
 /*
 #pragma mark - Navigation
 
